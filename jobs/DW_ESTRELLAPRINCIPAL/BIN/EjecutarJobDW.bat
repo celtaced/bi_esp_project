2acb3c60ea0ef93302af6784bbeb3fec95813644
Carlos Dubón <celtaced@gmail.com>
2022-05-15 20:48:01 -0600

    Version Final
    Version final del proyecto. Fue presentado a jurado.

--- jobs/DW_ESTRELLAPRINCIPAL/BIN/EjecutarJobDW.bat
+++ jobs/DW_ESTRELLAPRINCIPAL/BIN/EjecutarJobDW.bat
@@ -7,5 +7,5 @@ set minuto=%time:~3,2%
 set segundo=%time:~6,2%
 
 cd "C:\Apps\data-integration"
-call Kitchen.bat /file:C:\Apps\bi_esp_project\jobs\DW_ESTRELLAPRINCIPAL\LIB\JB_ESTRELLAPRINCIPAL.kjb -logfile=C:\Apps\bi_esp_project\jobs\DW_ESTRELLAPRINCIPAL\LOG\logDW.log
+call Kitchen.bat /file:C:\Apps\bi_esp_project\jobs\DW_ESTRELLAPRINCIPAL\LIB\JB_ESTRELLAPRINCIPAL.kjb -logfile=C:\Apps\bi_esp_project\jobs\DW_ESTRELLAPRINCIPAL\LOG\logDW_%date:~-4,4%%date:~-7,2%%date:~-10,2%.log
 exit
---
+++ jobs/DW_PREDICCIONVENTAS/BIN/EjecutarJobDW.bat
@@ -0,0 +1,11 @@
+set anio=%date:~6,4%
+set mes=%date:~3,2%
+set dia=%date:~0,2%
+set hora=%time:~0,2%
+set hora=%hora: =0%
+set minuto=%time:~3,2%
+set segundo=%time:~6,2%
+
+cd "C:\Apps\data-integration"
+call Kitchen.bat /file:C:\Apps\bi_esp_project\jobs\DW_PREDICCIONVENTAS\LIB\JB_PREDICCION.kjb -logfile=C:\Apps\bi_esp_project\jobs\DW_PREDICCIONVENTAS\LOG\logDW_%date:~-4,4%%date:~-7,2%%date:~-10,2%.log
+exit
